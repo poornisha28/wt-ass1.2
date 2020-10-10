256a45eeda7936bf8559028517040e3589cd5e5e
Poornisha <56213802+poornisha28@users.noreply.github.com>
2020-10-10 17:34:16 +0530

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -2,8 +2,6 @@
 // optional controllers
 function HomeCtrl($scope, $http) {
 }
-function ProjectCtrl($scope, $http) {
-}
 function PrivacyCtrl($scope, $http, $timeout) {
 }
 function AboutCtrl($scope, $http, $timeout) {
